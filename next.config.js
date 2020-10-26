76bb0e5a96e6c8f4616b6f3e66e4b8ba011376e8
Dwight Ian De Jesus <ddejesus0320@gmail.com>
2020-10-27 01:06:28 +0800

    Edit for Netlify

---
+++ next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    // Make sure that your node enviroment supports async/await
+    exportPathMap: async function (defaultPathMap) {
+        const path = {
+            "/": {
+                page: "/",
+            },
+            "/about": {
+                page: "/about",
+            },
+            "/team": {
+                page: "/team",
+            },
+            "/projects": {
+                page: "/projects",
+            },
+            "/blogs": {
+                page: "/blogs",
+            },
+            "/join-us": {
+                page: "/join-us",
+            },
+        };
+
+        return path;
+    },
+};
